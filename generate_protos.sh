97b924e8dcabcbdf6c3713c9c70194242e8c01bf
Katrina Rogan <krogan@lyft.com>
2019-03-04 16:37:31 -0800

    Refactor codegen (#2)
    api/core/v1/generated.proto imports apimachinery protos as k8s.io/apimachinery. Originally I mapped the submodule path for kubernetes/apimachinery to k8s.io/apimachinery but then this dirties the submodule git status when generating protos. 
    
    This change simply keeps kubernetes/aimachinery as the path and uses k8s.io/apimachinery for codegen which is then remapped to k8s/io/apimachinery for python import compatibility.
    
    Tested with
    ```
    from gen.api.core.v1.generated_pb2 import (
        Pod, PodSpec, Container, ResourceRequirements,
    )
    
    from k8s.io.apimachinery.pkg.api.resource.generated_pb2 import Quantity
    from k8s.io.apimachinery.pkg.apis.meta.v1.generated_pb2 import ObjectMeta
    
    demo_container = Container(
        name="memory-demo-ctr",
        image="polinux/stress",
        resources=ResourceRequirements(
            limits={
                "memory": Quantity(
                    string="200Mi",
                )},
            requests={
                "memory": Quantity(
                    string="100Mi",
                )},
        ),
        command=["stress"],
        args=["--vm", "1", "--vm-bytes", "150M", "--vm-hang", "1"]
    )
    
    specky = PodSpec(
        containers=[demo_container]
    )
    
    pod = Pod(
        metadata=ObjectMeta(
            name="memory-demo",
            namespace="mem-example",
        ),
        spec=specky,
    )
    
    print("{}".format(pod.SerializeToString()))
    ```
    
    and also tested that serialize proto the serialized proto could be unserialized to the corresponding go struct, a `k8s.io/api/core/v1` Pod

--- generate_protos.sh
+++ generate_protos.sh
@@ -1,2 +1,41 @@
 DIR=`pwd`
-docker run -v $DIR:/defs namely/protoc-all -d api/core/v1/ -l python -i .
+
+# Before running this make sure to init git submodules:
+# git submodule init
+# git submodule update
+
+rm -rf k8s.io/
+
+# Copy over all the kubernetes/apimachinery protos to where api/core/v1/generated.proto expects (k8s.io)
+mkdir -p k8s.io/apimachinery/pkg/runtime/
+cp kubernetes/apimachinery/pkg/runtime/generated.proto k8s.io/apimachinery/pkg/runtime/generated.proto
+mkdir -p k8s.io/apimachinery/pkg/api/resource/
+cp kubernetes/apimachinery/pkg/api/resource/generated.proto k8s.io/apimachinery/pkg/api/resource/generated.proto
+mkdir -p k8s.io/apimachinery/pkg/util/intstr/
+cp kubernetes/apimachinery/pkg/util/intstr/generated.proto k8s.io/apimachinery/pkg/util/intstr/generated.proto
+mkdir -p k8s.io/apimachinery/pkg/runtime/schema/
+cp kubernetes/apimachinery/pkg/runtime/schema/generated.proto k8s.io/apimachinery/pkg/runtime/schema/generated.proto
+mkdir -p k8s.io/apimachinery/pkg/apis/meta/v1/
+cp kubernetes/apimachinery/pkg/apis/meta/v1/generated.proto k8s.io/apimachinery/pkg/apis/meta/v1/generated.proto
+
+
+# Generate imported protos
+docker run -v $DIR:/defs namely/protoc-all -f k8s.io/apimachinery/pkg/runtime/generated.proto -l python -o gen
+docker run -v $DIR:/defs namely/protoc-all -f k8s.io/apimachinery/pkg/api/resource/generated.proto -l python -o gen
+docker run -v $DIR:/defs namely/protoc-all -f k8s.io/apimachinery/pkg/runtime/schema/generated.proto -l python -o gen
+docker run -v $DIR:/defs namely/protoc-all -f k8s.io/apimachinery/pkg/util/intstr/generated.proto -l python -o gen
+docker run -v $DIR:/defs namely/protoc-all -f k8s.io/apimachinery/pkg/apis/meta/v1/generated.proto -l python -o gen
+
+# Generate core proto
+docker run -v $DIR:/defs namely/protoc-all -f api/core/v1/generated.proto -l python -i . -o gen
+
+# python is a pain with imports when it comes to filenames with "." in them
+rm -rf k8s/io
+mkdir -p k8s/io
+mv gen/k8s/io/* k8s/io
+touch k8s/__init__.py
+
+# Clean up intermediate directories and files
+rm -rf k8s.io/
+rm -rf gen/k8s
+rm -rf gen/k8s.io
