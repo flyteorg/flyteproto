2847efce28d9eb86fed332ed12ff5c698ceb58d3
Katrina Rogan <krogan@lyft.com>
2019-04-16 13:15:50 -0700

    Add k8s python packaging (#10)

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="k8s-proto",
+    version="0.0.1",
+    maintainer="lyft",
+    maintainer_email='dev@lyft.com',
+    description="Generated kubernetes proto code",
+    license="Apache License Version 2.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lyft/flyteproto",
+    packages=setuptools.find_packages(),
+    install_requires=[
+         "protobuf>=3.6.1,<3.7.0",
+    ],
+)
